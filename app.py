24a2ee1d02557ef2f3743c0908d81cdf52d5c4b2
Hxin <122002544+TheHxin@users.noreply.github.com>
2025-02-27 13:12:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import telebot
 import schedule
 import time
 
-bot = telebot.TeleBot("6981201641:AAHEfBoXqx-DtuS9xr6v6QiheXyfUYLPDbk")
+chat_id = "replace with chatid [int]" 
+bot = telebot.TeleBot("api_key")
 
 def counter():
     with open("data","r") as file:
@@ -14,8 +15,8 @@ def counter():
 def get_quote():
     return pyquotegen.get_quote()
 def task():
-    bot.send_message(-1002017941746,f"{counter()} Days remain from the highschool")
-    bot.send_message(-1002017941746,f"Today's quote is '*{get_quote()}*'",parse_mode="Markdown")
+    bot.send_message(chat_id ,f"{counter()} Days remain from the highschool")
+    bot.send_message(chat_id ,f"Today's quote is '*{get_quote()}*'",parse_mode="Markdown")
 
 schedule.every().day.at("10:00").do(task)
 
